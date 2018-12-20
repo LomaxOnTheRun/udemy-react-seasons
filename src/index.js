082b0b2486a7431b1efc11ea1708c7e6f4e94b82
LomaxOnTheRun <lomax.on.the.run@gmail.com>
2018-12-20 21:10:22 +1100

    Update state with latitude

--- src/index.js
+++ src/index.js
@@ -2,13 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+
+    this.state = { latitude: null };
+
     window.navigator.geolocation.getCurrentPosition(
-      position => console.log(position),
+      position => this.setState({ latitude: position.coords.latitude }),
       error => console.log(error)
     );
+  }
 
-    return <div>Latitude: </div>;
+  render() {
+    return <div>Latitude: {this.state.latitude}</div>;
   }
 }
 
