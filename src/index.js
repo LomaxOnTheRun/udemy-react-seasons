78ed625af8621a9f9898b89104a3fc5fb9916fed
LomaxOnTheRun <lomax.on.the.run@gmail.com>
2018-12-21 11:23:44 +1100

    Move geolocation code to componentDidMount

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ class App extends React.Component {
     super(props);
 
     this.state = { latitude: null, errorMessage: "" };
+  }
 
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       position => this.setState({ latitude: position.coords.latitude }),
       error => this.setState({ errorMessage: error.message })
